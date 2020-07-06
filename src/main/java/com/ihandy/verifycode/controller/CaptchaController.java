ba58ec1dfdd3e498bc3f77df255bc80139cd70d5
lyc <1174510490@qq.com>
2020-07-06 18:48:32 +0800

    行为验证码

--- src/main/java/com/ihandy/verifycode/controller/CaptchaController.java
+++ src/main/java/com/ihandy/verifycode/controller/CaptchaController.java
@@ -3,8 +3,6 @@ package com.ihandy.verifycode.controller;
 import com.ihandy.verifycode.model.CaptchaModel;
 import com.ihandy.verifycode.service.CaptchaService;
 import com.ihandy.verifycode.utils.ResponseData;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
 import org.springframework.web.bind.annotation.*;
 import javax.annotation.Resource;
 
